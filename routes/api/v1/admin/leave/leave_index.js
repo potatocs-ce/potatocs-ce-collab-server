ed9cfa2b8e19e0861ec8c7dba6ef056d8a6636f0
BAEKSEONGYUN <baek@nsmarts.co.kr>
2023-07-21 17:53:01 +0900

    admin approval controller test api complete

--- routes/api/v1/admin/leave/leave_index.js
+++ routes/api/v1/admin/leave/leave_index.js
@@ -19,7 +19,6 @@ router.get('/getAdminMain', adMainCtrl.getAdminMain);
     회사 등록 승인 요청 리스트
 -----------------------------------*/
 router.get('/getPendingRequest', adApprovalCtrl.getPendingRequest);
-router.get('/getPendingRequestTest', adApprovalCtrl.getPendingRequestTest);
 router.put('/approveRequest', adApprovalCtrl.approveRequest);
 router.delete('/deleteRequest', adApprovalCtrl.deleteRequest);
 
