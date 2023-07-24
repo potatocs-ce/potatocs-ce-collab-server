c1c1f115219747949a400ebf0f20cd25c9e0e01f
park9379 <109051896+park9379@users.noreply.github.com>
2023-07-24 16:40:02 +0900

    add space_list schema at ad_menu_side_schema

--- models/ad_menu_side_schema.js
+++ models/ad_menu_side_schema.js
@@ -1,29 +1,30 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const adMenuSide = mongoose.Schema( 
+const adMenuSide = mongoose.Schema({
+  admin_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Admin",
+    required: true,
+  },
+  folder_list: [
     {
-        admin_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Admin',
-            required: true
-        },
-        folder_list: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Folder',
-            },
-        ],
-        space_list: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Space',
-            }
-        ]
-       
-    }
-);
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Folder",
+    },
+  ],
+  space_list: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Space",
+    },
+  ],
+  //박재현
+  isDialog: {
+    type: Boolean,
+  },
+  //end
+});
 
-
-const AdMenuSide = mongoose.model('AdMenuSide', adMenuSide);
+const AdMenuSide = mongoose.model("AdMenuSide", adMenuSide);
 
 module.exports = AdMenuSide;
