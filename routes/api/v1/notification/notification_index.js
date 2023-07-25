5aaec2bab83ea04a094b28b9940b63c49d581265
park9379 <109051896+park9379@users.noreply.github.com>
2023-07-25 16:39:50 +0900

    add GET api at notification

--- routes/api/v1/notification/notification_index.js
+++ routes/api/v1/notification/notification_index.js
@@ -1,15 +1,16 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
 /*-----------------------------------
 	Contollers
 -----------------------------------*/
-const notificationCtrl = require('./notification_controller');
+const notificationCtrl = require("./notification_controller");
 
 /*-----------------------------------
 	Notification
 -----------------------------------*/
-router.get('/get', notificationCtrl.getNotificationList);
-router.post('/edit', notificationCtrl.editNotification);
-router.get('/allRead', notificationCtrl.allReadNotification);
+router.get("/get", notificationCtrl.getNotificationList);
+router.post("/edit", notificationCtrl.editNotification);
+router.get("/allRead", notificationCtrl.allReadNotification);
+roguet.get("/Notice", notificationCtrl.allNotice);
 
 module.exports = router;
