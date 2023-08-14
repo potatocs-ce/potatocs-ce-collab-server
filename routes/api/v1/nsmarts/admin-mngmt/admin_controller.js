7e967d16280c9b875144353c48ace0f021443833
park9379 <109051896+park9379@users.noreply.github.com>
2023-08-14 16:06:53 +0900

    edit getAdminlist error mesage at admin_controller

--- routes/api/v1/nsmarts/admin-mngmt/admin_controller.js
+++ routes/api/v1/nsmarts/admin-mngmt/admin_controller.js
@@ -1,109 +1,96 @@
-const { ObjectId } = require('bson');
+const { ObjectId } = require("bson");
 
 exports.getAdminList = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
   User : ${req.decoded._id}
   API  : Get Admin List Info
   router.get('/getAdminList', adminMngmtCtrl.getAdminList);
   
 --------------------------------------------------`);
-    const dbModels = global.DB_MODELS;
+  const dbModels = global.DB_MODELS;
 
-    try {
-        const adminList = await dbModels.Admin.find().populate('company_id');
+  try {
+    const adminList = await dbModels.Admin.find().populate("company_id");
 
-        // console.log(adminList);
+    // console.log(adminList);
 
-        return res.status(200).send({
-            message: 'get admin list',
-            adminList
-        })
-
-
-    } catch (err) {
-        console.log('[ ERROR ]', err);
-        res.status(500).send({
-            message: 'get admin list Error'
-        })
-    }
+    return res.status(200).send({
+      message: "get admin list",
+      adminList,
+    });
+  } catch (err) {
+    console.log("[ ERROR ]", err);
+    res.status(404).send({
+      message: "get admin list Error",
+    });
+  }
 };
 
-
-
 exports.connectAdminCompany = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
   User : ${req.decoded._id}
   API  : Connect company and admin
   router.put('/connectAdminCompany', adminMngmtCtrl.connectAdminCompany);
   
 --------------------------------------------------`);
-    const dbModels = global.DB_MODELS;
-    const data = req.body;
-    console.log(data);
-    try {
+  const dbModels = global.DB_MODELS;
+  const data = req.body;
+  console.log(data);
+  try {
+    const adminInfo = await dbModels.Admin.findOneAndUpdate(
+      {
+        _id: data.admin_id,
+      },
+      {
+        company_id: data.company_id,
+      },
+      {
+        new: true,
+      }
+    );
+    console.log(adminInfo);
 
-        const adminInfo = await dbModels.Admin.findOneAndUpdate(
-            {
-                _id : data.admin_id
-            },
-            {
-                company_id : data.company_id,
-            },
-            {
-				new: true
-			}
-        )
-        console.log(adminInfo);
-
-        return res.status(200).send({
-            message: 'connect company and admin',
-        })
-
-
-    } catch (err) {
-
-        console.log('[ ERROR ]', err);
-        res.status(500).send({
-            message: 'connect company and admin Error'
-        })
-    }
+    return res.status(200).send({
+      message: "connect company and admin",
+    });
+  } catch (err) {
+    console.log("[ ERROR ]", err);
+    res.status(500).send({
+      message: "connect company and admin Error",
+    });
+  }
 };
 
 exports.disconnectAdminCompany = async (req, res) => {
-	console.log(`
+  console.log(`
 --------------------------------------------------
   User : ${req.decoded._id}
   API  : Connect company and admin
   router.put('/disconnectAdminCompany', adminMngmtCtrl.disconnectAdminCompany);
   
 --------------------------------------------------`);
-    const dbModels = global.DB_MODELS;
-    const data = req.body;
-    console.log(data);
-    try {
+  const dbModels = global.DB_MODELS;
+  const data = req.body;
+  console.log(data);
+  try {
+    await dbModels.Admin.findOneAndUpdate(
+      {
+        _id: data.admin_id,
+      },
+      {
+        company_id: null,
+      }
+    );
 
-        await dbModels.Admin.findOneAndUpdate(
-            {
-                _id: data.admin_id
-            },
-            {
-                company_id : null
-            }
-        )
-        
-
-        return res.status(200).send({
-            message: 'disconnect company and admin',
-        })
-
-
-    } catch (err) {
-
-        console.log('[ ERROR ]', err);
-        res.status(500).send({
-            message: 'disconnect company and admin Error'
-        })
-    }
+    return res.status(200).send({
+      message: "disconnect company and admin",
+    });
+  } catch (err) {
+    console.log("[ ERROR ]", err);
+    res.status(500).send({
+      message: "disconnect company and admin Error",
+    });
+  }
 };
