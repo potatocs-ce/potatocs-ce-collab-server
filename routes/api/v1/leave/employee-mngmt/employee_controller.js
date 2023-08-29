141bb67f0590845e09d2883029ab06b08ca6a8ac
limhokyun <lim@nsmarts.co.kr>
2023-08-29 15:21:22 +0900

    employee_controller change

--- routes/api/v1/leave/employee-mngmt/employee_controller.js
+++ routes/api/v1/leave/employee-mngmt/employee_controller.js
@@ -49,6 +49,38 @@ exports.getPendingList = async (req, res) => {
 			}
 		]);
 
+		const pendingCompanyData = await dbModels.PendingCompanyRequest.aggregate([
+			{
+				$match: {
+					member_id: ObjectId(req.decoded._id)
+				}
+			},
+			{
+				$lookup: {
+					from: 'companies',
+					localField: 'company_id',
+					foreignField: '_id',
+					as: 'company'
+				}
+			},
+			{
+				$unwind: {
+					path: '$company',
+					preserveNullAndEmptyArrays: true
+				}
+			},
+			{
+				$project: {
+					request_id: '$_id',
+					_id: '$company._id',
+					company_code: '$company.company_code',
+					company_name: '$company.company_name',
+					status: 1,
+				}
+			}
+		]);
+
+
 		return res.status(200).send({
 			message: 'found',
 			pendingList
@@ -173,269 +205,270 @@ exports.myEmployeeList = async (req, res) => {
 
 
 		const manager = await dbModels.Manager.find(
-            {
-                myManager: ObjectId(req.decoded._id)
-            },
-            {
-                myId: 1,
-                accepted: 1,
-            }
-        ).lean()
-        // console.log(manager);
-        const mngEmployee = [];
+			{
+				myManager: ObjectId(req.decoded._id)
+			},
+			{
+				myId: 1,
+				accepted: 1,
+			}
+		).lean()
+		// console.log(manager);
+		const mngEmployee = [];
 
-        for (let index = 0; index < manager.length; index++) {
-            const element = manager[index].myId;
-            mngEmployee.push(element);
-        }
+		for (let index = 0; index < manager.length; index++) {
+			const element = manager[index].myId;
+			mngEmployee.push(element);
+		}
 
 		const myEmployeeList = await dbModels.Member.aggregate([
-            {
-                $match: {
-                    _id: { $in: mngEmployee },
+			{
+				$match: {
+					_id: { $in: mngEmployee },
 					retired: false
-                }
-            },
-            {
-                $lookup: {
-                    from: 'personalleavestandards',
-                    localField: '_id',
-                    foreignField: 'member_id',
-                    as: 'totalLeave'
-                }
-            },
-            {
-                $addFields: {
-                    // year: {
-                    //     $floor: {
-                    //         $let: {
-                    //             vars: {
-                    //                 diff: {
-                    //                     $subtract: [new Date(), "$emp_start_date"]
-                    //                 }
-                    //             },
-                    //             in: {
-                    //                 $divide: ["$$diff", (365 * 24 * 60 * 60 * 1000)]
-                    //             }
-                    //         }
-                    //     }
-                    // }
+				}
+			},
+			{
+				$lookup: {
+					from: 'personalleavestandards',
+					localField: '_id',
+					foreignField: 'member_id',
+					as: 'totalLeave'
+				}
+			},
+			{
+				$addFields: {
+					// year: {
+					//     $floor: {
+					//         $let: {
+					//             vars: {
+					//                 diff: {
+					//                     $subtract: [new Date(), "$emp_start_date"]
+					//                 }
+					//             },
+					//             in: {
+					//                 $divide: ["$$diff", (365 * 24 * 60 * 60 * 1000)]
+					//             }
+					//         }
+					//     }
+					// }
 
-                    // dateDiff : 년차를 계산 -> 단 년만 보고 계산함 월이랑 일은 생각안함
-                    dateDiff: {
-                        $dateDiff:{
-                            startDate: "$emp_start_date",
-                            endDate: "$$NOW",
-                            unit: "year"
-                        }
-                    },
+					// dateDiff : 년차를 계산 -> 단 년만 보고 계산함 월이랑 일은 생각안함
+					dateDiff: {
+						$dateDiff: {
+							startDate: "$emp_start_date",
+							endDate: "$$NOW",
+							unit: "year"
+						}
+					},
 
-                    // dateCompare : 그래서 이 친구가 필요, 이 친구가 현재와 계약 달, 일을 비교해서 1을 빼줄지 말지 정해줌 -> 오류
-                    // 오류 수정을 위한 수정된 코드
-                    // 달끼리 일끼리 비교하니까 오류가 나서 달일 달일 로 비교하기 위한 방법
-                    emp_start : {
-                        $dateFromParts: {
-                          'year': {$year : '$$NOW'}, 'month' : {$month : '$$NOW'}, 'day' : {$dayOfMonth: '$$NOW'}
-                        }
-                      },
-                      now_date : {
-                        $dateFromParts: {
-                          'year': {$year : '$$NOW'}, 'month' : {$month:'$emp_start_date'}, 'day' : {$dayOfMonth: '$emp_start_date'}
-                        }
-                      },
-                }
-            },
-            {
-                // 위의 emp_start, now_date 를 가지고 dateCompare
-                $addFields: {
-                    dateCompare : {
-                        $cond:[{
-                            $and: [
-                              // {$gte: [ {$month: '$$NOW'}, {$month:'$emp_start_date'}]},
-                              // {$gte: [ {$dayOfMonth :"$$NOW"}, {$dayOfMonth: '$emp_start_date'}]},
-                              {$gte: [ '$emp_start', '$now_date'  ] },
-                            ]}, 0,1
-                        ]
-                    },
-                }
-            },
-            {   // dateDiff 와 dateCompare 의 차 를 year로
-                $addFields: {
-                    year: {
-                        $subtract: ["$dateDiff", "$dateCompare"]
-                    }
-                }
-            },
-            {
-                $lookup: {
-                    from: 'leaverequests',
-                    let: {
-                        userId: '$_id',
-                        years: '$year',
-                    },
-                    pipeline: [
-                        {
-                            $match: {
-                                $expr: {
-                                    $and: [
-                                        { $eq: ["$requestor", "$$userId"] },
-                                        { $eq: ["$year", "$$years"] }
+					// dateCompare : 그래서 이 친구가 필요, 이 친구가 현재와 계약 달, 일을 비교해서 1을 빼줄지 말지 정해줌 -> 오류
+					// 오류 수정을 위한 수정된 코드
+					// 달끼리 일끼리 비교하니까 오류가 나서 달일 달일 로 비교하기 위한 방법
+					emp_start: {
+						$dateFromParts: {
+							'year': { $year: '$$NOW' }, 'month': { $month: '$$NOW' }, 'day': { $dayOfMonth: '$$NOW' }
+						}
+					},
+					now_date: {
+						$dateFromParts: {
+							'year': { $year: '$$NOW' }, 'month': { $month: '$emp_start_date' }, 'day': { $dayOfMonth: '$emp_start_date' }
+						}
+					},
+				}
+			},
+			{
+				// 위의 emp_start, now_date 를 가지고 dateCompare
+				$addFields: {
+					dateCompare: {
+						$cond: [{
+							$and: [
+								// {$gte: [ {$month: '$$NOW'}, {$month:'$emp_start_date'}]},
+								// {$gte: [ {$dayOfMonth :"$$NOW"}, {$dayOfMonth: '$emp_start_date'}]},
+								{ $gte: ['$emp_start', '$now_date'] },
+							]
+						}, 0, 1
+						]
+					},
+				}
+			},
+			{   // dateDiff 와 dateCompare 의 차 를 year로
+				$addFields: {
+					year: {
+						$subtract: ["$dateDiff", "$dateCompare"]
+					}
+				}
+			},
+			{
+				$lookup: {
+					from: 'leaverequests',
+					let: {
+						userId: '$_id',
+						years: '$year',
+					},
+					pipeline: [
+						{
+							$match: {
+								$expr: {
+									$and: [
+										{ $eq: ["$requestor", "$$userId"] },
+										{ $eq: ["$year", "$$years"] }
 
-                                    ]
-                                }
-                            }
-                        },
-                        {
-                            $facet: {
-                                used_annual_leave: [
-                                    {
-                                        $match: {
-                                            $expr: {
-												$and:[
-													{$eq: ["$leaveType", 'annual_leave']},
+									]
+								}
+							}
+						},
+						{
+							$facet: {
+								used_annual_leave: [
+									{
+										$match: {
+											$expr: {
+												$and: [
+													{ $eq: ["$leaveType", 'annual_leave'] },
 													{
-														$or:[
-															{$eq: ["$status", 'approve']},
-															{$eq: ["$status", 'pending']}
-														]
-													}
-												]
-                                            }
-                                        }
-                                    },
-                                    {
-                                        $group: {
-                                            _id: null,
-                                            sum: {
-                                                "$sum": "$leaveDuration"
-                                            }
-                                        }
-                                    }
-                                ],
-								used_rollover: [
-                                    {
-                                        $match: {
-                                            $expr: {
-												$and:[
-													{$eq: ["$leaveType", 'rollover']},
-													{
-														$or:[
-															{$eq: ["$status", 'approve']},
-															{$eq: ["$status", 'pending']}
-														]
-													}
-												]
-                                            }
-                                        }
-                                    },
-                                    {
-                                        $group: {
-                                            _id: null,
-                                            sum: {
-                                                "$sum": "$leaveDuration"
-                                            }
-                                        }
-                                    }
-                                ],
-                                used_sick_leave: [
-                                    {
-                                        $match: {
-                                            $expr: {
-                                                $and:[
-													{$eq: ["$leaveType", 'sick_leave']},
-													{
-														$or:[
-															{$eq: ["$status", 'approve']},
-															{$eq: ["$status", 'pending']}
+														$or: [
+															{ $eq: ["$status", 'approve'] },
+															{ $eq: ["$status", 'pending'] }
 														]
 													}
 												]
 											}
-                                        }
-                                    },
-                                    {
-                                        $group: {
-                                            _id: null,
-                                            sum: {
-                                                "$sum": "$leaveDuration"
-                                            }
-                                        }
-                                    }
-                                ],
-                                used_replacement_leave: [
-                                    {
-                                        $match: {
-                                           $expr: {
-												$and:[
-													{$eq: ["$leaveType", 'replacement_leave']},
+										}
+									},
+									{
+										$group: {
+											_id: null,
+											sum: {
+												"$sum": "$leaveDuration"
+											}
+										}
+									}
+								],
+								used_rollover: [
+									{
+										$match: {
+											$expr: {
+												$and: [
+													{ $eq: ["$leaveType", 'rollover'] },
 													{
-														$or:[
-															{$eq: ["$status", 'approve']},
-															{$eq: ["$status", 'pending']}
+														$or: [
+															{ $eq: ["$status", 'approve'] },
+															{ $eq: ["$status", 'pending'] }
 														]
 													}
 												]
-                                            }
-                                        }
-                                    },
-                                    {
-                                        $group: {
-                                            _id: null,
-                                            sum: {
-                                                "$sum": "$leaveDuration"
-                                            }
-                                        }
-                                    }
-                                ]
-                            }
-                        }
-                    ]
-                    , as: "usedLeave"
-                }
-            },
-            {
-                $unwind: {
-                    path: '$totalLeave',
-                    preserveNullAndEmptyArrays: true
-                }
-            },
-            {
-                $unwind: {
-                    path: '$usedLeave',
-                    preserveNullAndEmptyArrays: true
-                }
-            },
+											}
+										}
+									},
+									{
+										$group: {
+											_id: null,
+											sum: {
+												"$sum": "$leaveDuration"
+											}
+										}
+									}
+								],
+								used_sick_leave: [
+									{
+										$match: {
+											$expr: {
+												$and: [
+													{ $eq: ["$leaveType", 'sick_leave'] },
+													{
+														$or: [
+															{ $eq: ["$status", 'approve'] },
+															{ $eq: ["$status", 'pending'] }
+														]
+													}
+												]
+											}
+										}
+									},
+									{
+										$group: {
+											_id: null,
+											sum: {
+												"$sum": "$leaveDuration"
+											}
+										}
+									}
+								],
+								used_replacement_leave: [
+									{
+										$match: {
+											$expr: {
+												$and: [
+													{ $eq: ["$leaveType", 'replacement_leave'] },
+													{
+														$or: [
+															{ $eq: ["$status", 'approve'] },
+															{ $eq: ["$status", 'pending'] }
+														]
+													}
+												]
+											}
+										}
+									},
+									{
+										$group: {
+											_id: null,
+											sum: {
+												"$sum": "$leaveDuration"
+											}
+										}
+									}
+								]
+							}
+						}
+					]
+					, as: "usedLeave"
+				}
+			},
 			{
-                $lookup: {
-                    from: 'nationalholidays',
-                    localField: 'location',
-                    foreignField: '_id',
-                    as: 'countryName'
-                }
-            },
-            {
-                $unwind: {
-                    path: '$countryName',
-                    preserveNullAndEmptyArrays: true
-                }
-            },
-            {
-                $project: {
-                    _id: 1,
-                    name: 1,
-                    year: 1,
-                    position: 1,
-                    location: '$countryName.countryName',
-                    emp_start_date: 1,
-                    emp_end_date: 1,
-                    isManager: 1,
-                    totalLeave: {
-                        $arrayElemAt: ["$totalLeave.leave_standard", "$year"]
-                    },
-                    usedLeave: 1
-                }
-            }
-        ]);
+				$unwind: {
+					path: '$totalLeave',
+					preserveNullAndEmptyArrays: true
+				}
+			},
+			{
+				$unwind: {
+					path: '$usedLeave',
+					preserveNullAndEmptyArrays: true
+				}
+			},
+			{
+				$lookup: {
+					from: 'nationalholidays',
+					localField: 'location',
+					foreignField: '_id',
+					as: 'countryName'
+				}
+			},
+			{
+				$unwind: {
+					path: '$countryName',
+					preserveNullAndEmptyArrays: true
+				}
+			},
+			{
+				$project: {
+					_id: 1,
+					name: 1,
+					year: 1,
+					position: 1,
+					location: '$countryName.countryName',
+					emp_start_date: 1,
+					emp_end_date: 1,
+					isManager: 1,
+					totalLeave: {
+						$arrayElemAt: ["$totalLeave.leave_standard", "$year"]
+					},
+					usedLeave: 1
+				}
+			}
+		]);
 
 
 		// console.log(myEmployeeList)
@@ -507,7 +540,7 @@ exports.UpdateEmployeeInfo = async (req, res) => {
 		const criteria = {
 			_id: req.body.employeeId
 		}
-		
+
 		const updateData = {
 			name: req.body.name,
 			position: req.body.position,
@@ -548,7 +581,7 @@ exports.myEmployeeLeaveListSearch = async (req, res) => {
   router.get('/myEmployee-leaveList', employeeMngmtCtrl.myEmployeeLeaveList);
 --------------------------------------------------`);
 
-    const data = req.query;
+	const data = req.query;
 	// console.log(data.emailFind);
 	if (data.emailFind == '' || data.emailFind == 'null') {
 		data.emailFind = 'all';
@@ -615,7 +648,7 @@ exports.myEmployeeLeaveListSearch = async (req, res) => {
 			},
 			{
 				$project: {
-                    requestId: '$leave._id',
+					requestId: '$leave._id',
 					name: '$memberName.name',
 					duration: '$leave.leaveDuration',
 					leaveType: '$leave.leaveType',
