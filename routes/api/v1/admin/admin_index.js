95032e3317a7797de664f78d34d606aaecf613c3
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-06-21 10:34:01 +0900

    [feat] emplyment contract API 수정

--- routes/api/v1/admin/admin_index.js
+++ routes/api/v1/admin/admin_index.js
@@ -1,49 +1,47 @@
-const router = require('express').Router();
-const multer = require('multer');
+const router = require("express").Router();
+const multer = require("multer");
 const { profileUpload } = require("../../../../utils/s3Utils");
 
 /*-----------------------------------
   ADMIN TOP TIER FOLDER
 -----------------------------------*/
 
-const leaves = require('./leaves/leaves_index')
+const leaves = require("./leaves/leaves_index");
 
 /*-----------------------------------
   INDEXES
 -----------------------------------*/
-const employees = require('./employees/employees_index');
+const employees = require("./employees/employees_index");
+const employment_contracts = require("./employment_contracts/employment_contracts_index");
 
 /*-----------------------------------
   Controller
 -----------------------------------*/
-const adProfileCtrl = require('./adProfile/adProfile_controller');
-
-
+const adProfileCtrl = require("./adProfile/adProfile_controller");
 
 /*-----------------------------------
   API
 -----------------------------------*/
-router.use('/leaves', leaves);
-router.use('/employees', employees);
-
+router.use("/leaves", leaves);
+router.use("/employees", employees);
+router.use("/employment_contracts", employment_contracts);
 
 /* Profile Image Update */
 const storage = multer.diskStorage({
-  destination(req, file, cb) {
-    cb(null, 'uploads/profile_img/temp');
-  },
-  filename(req, file, cb) {
-    // fileName = encodeURI(file.originalname);
-    cb(null, `${Date.now()}_${file.originalname}`);
+    destination(req, file, cb) {
+        cb(null, "uploads/profile_img/temp");
+    },
+    filename(req, file, cb) {
+        // fileName = encodeURI(file.originalname);
+        cb(null, `${Date.now()}_${file.originalname}`);
 
-    // cb(null, `${file.originalname}`);
-  }
+        // cb(null, `${file.originalname}`);
+    },
 });
 const upload = multer({ storage });
 /* Profile */
-router.get('/profile', adProfileCtrl.profile);
-router.patch('/profileChange', adProfileCtrl.profileChange);
+router.get("/profile", adProfileCtrl.profile);
+router.patch("/profileChange", adProfileCtrl.profileChange);
 router.post("/profileImageChange", profileUpload.single("file"), adProfileCtrl.profileImageChange);
-
 
 module.exports = router;
