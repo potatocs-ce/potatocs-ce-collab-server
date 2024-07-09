7ac7726460e96b008a1caaf650217a8f93b2b796
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-07-09 15:54:57 +0900

    [fix] 안쓰는 코드 정리

--- routes/api/v1/admin/admin_index.js
+++ routes/api/v1/admin/admin_index.js
@@ -1,16 +1,5 @@
 const router = require("express").Router();
-const multer = require("multer");
-const { profileUpload } = require("../../../../utils/s3Utils");
 
-/*-----------------------------------
-  ADMIN TOP TIER FOLDER
------------------------------------*/
-
-const leaves = require("./leaves/leaves_index");
-
-/*-----------------------------------
-  INDEXES
------------------------------------*/
 const employees = require("./employees/employees_index");
 const retired_employees = require("./retired_employees/retired_employees_index");
 const employment_contracts = require("./employment_contracts/employment_contracts_index");
@@ -18,38 +7,11 @@ const holidays = require("./holidays/holidays_index");
 const dashboard = require("./dashboard/dashboard_index");
 const profiles = require("./profiles/profiles_index");
 
-/*-----------------------------------
-  Controller
------------------------------------*/
-const adProfileCtrl = require("./adProfile/adProfile_controller");
-
-/*-----------------------------------
-  API
------------------------------------*/
-router.use("/leaves", leaves);
 router.use("/employees", employees);
 router.use("/retired_employees", retired_employees);
 router.use("/employment_contracts", employment_contracts);
 router.use("/holidays", holidays);
 router.use("/dashboard", dashboard);
 router.use("/profiles", profiles);
-
-/* Profile Image Update */
-const storage = multer.diskStorage({
-    destination(req, file, cb) {
-        cb(null, "uploads/profile_img/temp");
-    },
-    filename(req, file, cb) {
-        // fileName = encodeURI(file.originalname);
-        cb(null, `${Date.now()}_${file.originalname}`);
-
-        // cb(null, `${file.originalname}`);
-    },
-});
-const upload = multer({ storage });
-/* Profile */
-// router.get("/profile", adProfileCtrl.profile);
-// router.patch("/profileChange", adProfileCtrl.profileChange);
-// router.post("/profileImageChange", profileUpload.single("file"), adProfileCtrl.profileImageChange);
 
 module.exports = router;
--- routes/api/v1/admin/dashboard/dashboard_index.js
+++ routes/api/v1/admin/dashboard/dashboard_index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 const dashboardCtrl = require("./dashboard_controller");
 
-// 휴일 목록
+// 대쉬보드
 router.get("/", dashboardCtrl.getDashboard);
 
 module.exports = router;
