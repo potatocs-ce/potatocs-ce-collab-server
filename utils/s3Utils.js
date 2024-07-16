a5d72fc200baa259d4eb6313ff91ab883e0be481
BAEKSEONGYUN <baek@nsmarts.co.kr>
2024-07-16 16:44:42 +0900

    s3 admin profile 수정

--- utils/s3Utils.js
+++ utils/s3Utils.js
@@ -1,7 +1,8 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const path = require('path');
-const { S3Client } = require("@aws-sdk/client-s3");
+const sharp = require('sharp');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const s3Client = new S3Client({
     region: process.env.AWS_REGION,
@@ -52,34 +53,86 @@ const storage = multerS3({
     },
 });
 
+// Multer 메모리 저장소 설정
+// 이미지 리사이즈를 위해 메모리에 저장 -> 리사이즈 -> s3 저장으로 코드 수정
+const memoryStorage = multer.memoryStorage();
+
 const upload = multer({
     storage: storage,
     fileFilter: fileFilter
 });
 
-const nsProfileUpload = multer({
-    storage: multer.diskStorage({
-        destination(req, file, cb) {
-            cb(null, "uploads/nsProfile_img/temp");
-        },
-        fileFilter: function (req, file, cb) {
-            const allowedMimes = ["image/jpeg", "image/png"];
-            if (allowedMimes.includes(file.mimetype)) {
-                cb(null, true);
-            } else {
-                cb(new Error("Invalid file type. Only JPEG and PNG are allowed."));
+const uploadMemory = multer({
+    storage: memoryStorage,
+    fileFilter: fileFilter
+})
+
+/**
+리사이즈 이미지
+*/
+const resizeAndUploadImage = async (req, res, next) => {
+    try {
+        req.uploadedImage = [];
+
+        if (!req.files || req.files.length === 0) {
+            return next();
+        }
+
+        for (const file of req.files) {
+            const uploadPath = file.fieldname;
+
+            const originalnameUtf8 = Buffer.from(file.originalname, 'latin1').toString('utf8');
+
+            // 파일이 이미지인지 확인
+            if (!file.mimetype.startsWith('image/')) {
+                throw new Error('Invalid file type. Only images are allowed for resizing.');
             }
-        },
-        metadata: function (req, file, cb) {
-            cb(null, { fieldName: file.fieldname });
-        },
-    }),
-});
+
+
+            // 이미지 리사이즈
+            const resizedImage = await sharp(file.buffer)
+                .resize(300) // 가로 크기 300px로 리사이즈
+                .toBuffer();
+
+            // 리사이즈된 이미지 저장 경로 설정
+            const filePath = `${uploadPath}/${Date.now().toString()}_${originalnameUtf8}`;
+
+            // S3에 업로드
+            await s3Client.send(new PutObjectCommand({
+                ACL: 'public-read',
+                Bucket: process.env.AWS_S3_BUCKET,
+                Key: filePath,
+                Body: resizedImage,
+                ContentType: file.mimetype,
+                Metadata: {
+                    fieldName: file.fieldname
+                }
+            }));
+
+            // 업로드된 파일 정보를 req 객체에 저장
+            req.uploadedImage.push({
+                fieldname: file.fieldname,
+                originalname: file.originalname,
+                mimetype: file.mimetype,
+                location: `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${filePath}`,
+                key: filePath
+            });
+
+        }
+        next();
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error processing file.');
+    }
+};
 
 
 const uploadAny = upload.any();
+const uploadImage = uploadMemory.any();
 
 module.exports = {
     uploadAny,
-    nsProfileUpload,
+    uploadImage,
+    s3Client,
+    resizeAndUploadImage
 };
