b44988e99ab0f083b4d41a712cd3cbe1dc34788b
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-08-12 15:47:06 +0900

    [fix]

--- routes/api/v1/employees/leaves/leaves_index.js
+++ routes/api/v1/employees/leaves/leaves_index.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const requests = require("./requests/requests_index");
-const status = require("./status/status_index");
+//const status = require("./status/status_index");
 
 /*-----------------------------------
   Contollers
