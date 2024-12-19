270cb1bf60ba82c56383fe86b0835a904502a80b
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 09:58:37 +0900

    ad menu 코드 정리

--- models/ad_menu_side_schema.js
+++ models/ad_menu_side_schema.js
@@ -1,29 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const adMenuSide = mongoose.Schema( 
-    {
-        admin_id: {
+const adMenuSide = mongoose.Schema({
+    admin_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Admin",
+        required: true,
+    },
+    folder_list: [
+        {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Admin',
-            required: true
+            ref: "Folder",
         },
-        folder_list: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Folder',
-            },
-        ],
-        space_list: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Space',
-            }
-        ]
-       
-    }
-);
+    ],
+    space_list: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Space",
+        },
+    ],
+});
 
-
-const AdMenuSide = mongoose.model('AdMenuSide', adMenuSide);
+const AdMenuSide = mongoose.model("AdMenuSide", adMenuSide);
 
 module.exports = AdMenuSide;
