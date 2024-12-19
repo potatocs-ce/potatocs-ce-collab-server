c26ca8d1e1a8214812eeaa70b748628e908226a9
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 10:05:39 +0900

    chat 코드 정리

--- models/chat_schema.js
+++ models/chat_schema.js
@@ -1,34 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const chatScehma = mongoose.Schema(
-	{
-		docId: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Document',
-		},
+    {
+        docId: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Document",
+        },
         chatMember: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Member',
+            ref: "Member",
         },
-		chatContent: {
-			type: String,
-		},
-		isDialog: {
-			type: Boolean
-		},
+        chatContent: {
+            type: String,
+        },
+        isDialog: {
+            type: Boolean,
+        },
         reply: {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Chat',
-        }
-        
-	},
-	{
-		timestamps: true
-	}
+            ref: "Chat",
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-const Chat = mongoose.model('Chat', chatScehma);
+const Chat = mongoose.model("Chat", chatScehma);
 
 module.exports = Chat;
-
-
