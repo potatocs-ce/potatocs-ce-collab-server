e7eeb76a001846ca389342dccd9490f34d4bc295
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 10:44:04 +0900

    national holiday 코드 정리

--- models/national_holiday_schema.js
+++ models/national_holiday_schema.js
@@ -1,30 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const nationalHoliday = mongoose.Schema( 
-    {
-        countryName: {
-            type: String,
-            required: true
+const nationalHoliday = mongoose.Schema({
+    countryName: {
+        type: String,
+        required: true,
+    },
+    countryCode: {
+        type: String,
+        required: true,
+    },
+    countryHoliday: [
+        {
+            holidayName: {
+                type: String,
+            },
+            holidayDate: {
+                type: String,
+            },
         },
-        countryCode: {
-            type: String,
-            required: true
-        },
-        countryHoliday: [
-            {
-                holidayName: {
-                    type: String
-                },
-                holidayDate: {
-                    type: String
-                }
-            }
-        ],
-       
-    }
-);
+    ],
+});
 
-
-const NationalHoliday = mongoose.model('NationalHoliday', nationalHoliday);
+const NationalHoliday = mongoose.model("NationalHoliday", nationalHoliday);
 
 module.exports = NationalHoliday;
