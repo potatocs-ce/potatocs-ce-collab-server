4e7ac12ae948c75e2000dc49545f09534f3a359e
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 16:02:55 +0900

    uploadfile 코드 정리

--- models/uploadFile_schema.js
+++ models/uploadFile_schema.js
@@ -1,42 +1,40 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const uploadFile = mongoose.Schema(
-  {
-    doc_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Document',
-    },
-    originalname: {
-      type: String
-    },
+    {
+        doc_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Document",
+        },
+        originalname: {
+            type: String,
+        },
 
-    destination: {
-      type: String
+        destination: {
+            type: String,
+        },
+        filename: {
+            type: String,
+        },
+        key: {
+            type: String,
+        },
+        fileType: {
+            type: String,
+        },
+        creator: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Member",
+        },
+        description: {
+            type: String,
+        },
     },
-    filename: {
-      type: String,
-    },
-    key: {
-      type: String
-    },
-    fileType: {
-      type: String
-    },
-    creator: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Member',
-    },
-    description: {
-      type: String
+    {
+        timestamps: true,
     }
-  },
-  {
-    timestamps: true
-  }
 );
 
-const UploadFile = mongoose.model('UploadFile', uploadFile);
+const UploadFile = mongoose.model("UploadFile", uploadFile);
 
 module.exports = UploadFile;
-
-
