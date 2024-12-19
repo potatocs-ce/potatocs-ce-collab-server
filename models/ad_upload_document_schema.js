3602ee7cfa7902b84ba4fcd6288df6f408a466e8
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 10:02:12 +0900

    ad upload document 코드 정리

--- models/ad_upload_document_schema.js
+++ models/ad_upload_document_schema.js
@@ -1,40 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const adUploadDocument = mongoose.Schema(
-  {
-    company_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Company',
+    {
+        company_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Company",
+        },
+        writer: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Admin",
+        },
+        title: {
+            type: String,
+        },
+        content: {
+            type: String,
+        },
+        originalname: {
+            type: String,
+        },
+        key: {
+            type: String,
+        },
+        location: {
+            type: String,
+        },
+        pdfHash: {
+            type: String,
+        },
     },
-    writer: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: 'Admin',
-    },
-    title: {
-      type: String
-    },
-    content: {
-      type: String
-    },
-    originalname: {
-      type: String
-    },
-    key: {
-      type: String,
-    },
-    location: {
-      type: String
-    },
-    pdfHash: {
-      type: String
+    {
+        timestamps: true,
     }
-  },
-  {
-    timestamps: true
-  }
 );
 
-
-const AdUploadDocument = mongoose.model('AdUploadDocument', adUploadDocument);
+const AdUploadDocument = mongoose.model("AdUploadDocument", adUploadDocument);
 
 module.exports = AdUploadDocument;
