1c062df300a4af944b7cff2d64f71d7b283a9e78
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 10:42:22 +0900

    menu side 코드 정리

--- models/menu_side_schema.js
+++ models/menu_side_schema.js
@@ -1,29 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const menuSide = mongoose.Schema( 
-    {
-        member_id: {
+const menuSide = mongoose.Schema({
+    member_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Member",
+        required: true,
+    },
+    folder_list: [
+        {
             type: mongoose.Schema.Types.ObjectId,
-            ref: 'Member',
-            required: true
+            ref: "Folder",
         },
-        folder_list: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Folder',
-            },
-        ],
-        space_list: [
-            {
-                type: mongoose.Schema.Types.ObjectId,
-                ref: 'Space',
-            }
-        ]
-       
-    }
-);
+    ],
+    space_list: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Space",
+        },
+    ],
+});
 
-
-const MenuSide = mongoose.model('MenuSide', menuSide);
+const MenuSide = mongoose.model("MenuSide", menuSide);
 
 module.exports = MenuSide;
