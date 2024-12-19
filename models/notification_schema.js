71d830d93bc5e15b27888e07f916c2b0e50b5122
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 10:46:10 +0900

    notification 코드 정리

--- models/notification_schema.js
+++ models/notification_schema.js
@@ -1,38 +1,36 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const notificationSchema = mongoose.Schema(
-	{
+    {
         sender: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Member',
-		},
-		receiver: {
-			type: mongoose.Schema.Types.ObjectId,
-			ref: 'Member',
-		},
-		notiType: {
-			type: String
-		},
-		isRead: {
-			type: Boolean
-		},
-		iconText: {
-            type: String
-		},
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Member",
+        },
+        receiver: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Member",
+        },
+        notiType: {
+            type: String,
+        },
+        isRead: {
+            type: Boolean,
+        },
+        iconText: {
+            type: String,
+        },
         notiLabel: {
-            type: String
+            type: String,
         },
         navigate: {
-            type: String
-        }
-	},
-	{
-		timestamps: true
-	}
+            type: String,
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
-const Notification = mongoose.model('Notification', notificationSchema);
+const Notification = mongoose.model("Notification", notificationSchema);
 
 module.exports = Notification;
-
-
