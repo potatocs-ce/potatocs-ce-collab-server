a7f04f82aef691c7301341111abc4c15b2b6dd31
nsmarts-jmh <mcho@nsmarts.co.kr>
2024-12-19 16:13:28 +0900

    contracts 코드 정리

--- routes/api/v1/admin/contracts/contracts_index.js
+++ routes/api/v1/admin/contracts/contracts_index.js
@@ -1,21 +1,20 @@
 const router = require("express").Router();
 const contractsController = require("./contracts_controller");
-const { uploadContract } = require('../../../../../utils/s3Utils');
-
+const { uploadContract } = require("../../../../../utils/s3Utils");
 
 /*-----------------------------------
   Admin Controller
     admin contract list 페이지
 -----------------------------------*/
-router.get('/getContractList', contractsController.getContractList);
-router.get('/getEmployeeList', contractsController.getEmployeeList);
-router.get('/searchContractor', contractsController.searchContractor);
-router.post('/saveContract', uploadContract.single('file'), contractsController.saveContract);
-router.get('/getContractInfo', contractsController.getContractInfo);
-router.get('/getPdfFile', contractsController.getPdfFile);
-router.post('/signContract', contractsController.signContract);
-router.post('/rejectContract', contractsController.rejectContract);
-router.get('/getContractListSearch', contractsController.getContractListSearch);
-router.get('/getSignInfo', contractsController.getSignInfo);
+router.get("/getContractList", contractsController.getContractList);
+router.get("/getEmployeeList", contractsController.getEmployeeList);
+router.get("/searchContractor", contractsController.searchContractor);
+router.post("/saveContract", uploadContract.single("file"), contractsController.saveContract);
+router.get("/getContractInfo", contractsController.getContractInfo);
+router.get("/getPdfFile", contractsController.getPdfFile);
+router.post("/signContract", contractsController.signContract);
+router.post("/rejectContract", contractsController.rejectContract);
+router.get("/getContractListSearch", contractsController.getContractListSearch);
+router.get("/getSignInfo", contractsController.getSignInfo);
 
 module.exports = router;
